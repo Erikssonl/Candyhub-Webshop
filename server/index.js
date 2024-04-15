1d6cf4c88648c3c4cedb4b9833b9759c52515285
Louise Eriksson <louiseriksson.92@gmail.com>
2024-04-15 13:42:06 +0200

    conection to server done

--- server/index.js
+++ server/index.js
@@ -1,10 +1,39 @@
 const express = require('express');
-const app = express();
+const mysql = require('mysql');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Create a MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'YiGdf?ysc?F$4HEy',
+  database: 'candy_hub_db',
+  port: 3006
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+// Connect to MySQL
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL database: ' + err.stack);
+    return;
+  }
+  console.log('Connected to MySQL database as id ' + connection.threadId);
+});
+
+const app = express();
+const port = 3000;
+
+app.get('/products', (req, res) => {
+  connection.query('SELECT * FROM products', (error, results) => {
+    if (error) {
+      console.error('Error executing MySQL query: ' + error.stack);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+    res.json(results);
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
