f55ce1729eb5ad76c2b68baa015e42d28bd3ef3d
Louise Eriksson <louiseriksson.92@gmail.com>
2024-08-21 11:25:09 +0200

    updated responsive layout

--- client/LE-candyhub-app/src/Components/CategoryComp.jsx
+++ client/LE-candyhub-app/src/Components/CategoryComp.jsx
@@ -24,23 +24,23 @@ const CategoryComp = () => {
       </div>
       <div className={styles.categoryWrap}>
         {categories.map((category, idx) => (
-          <div className={`card lg:card-side bg-base-100 shadow-xl m-4`} 
+          <div className={`card lg:card-side bg-base-100 shadow-xl m-4 lg:m-8`} 
             style={{ backgroundColor: categoryColors[category.replace(/\s+/g, '')] }}
             key={idx} onClick={() => handleCategoryClick(category)}>
-            <div className="card-body items-center text-center">
-              <figure className="px-5 pt-5">
+            <div className="card-body items-center text-center p-4 lg:p-8">
+              <figure className="px-4 pt-4 lg:px-8 lg:pt-8">
                 <img src={`./${category}.png`} alt={category} className="rounded-xl" />
               </figure>
-              <h3 className="card-title">{category}</h3>
+              <h3 className="card-title text-lg lg:text-xl">{category}</h3>
             </div>
           </div>
         ))}
-        <div className="card lg:card-side bg-base-100 shadow-xl m-4 bg-costomCardDarkGreen" onClick={handleShowAllCandy}>
-          <div className="card-body items-center text-center">
-            <figure className="px-5 pt-5">
+        <div className="card lg:card-side bg-base-100 shadow-xl m-4 lg:m-8 bg-costomCardDarkGreen" onClick={handleShowAllCandy}>
+          <div className="card-body items-center text-center p-4 lg:p-8">
+            <figure className="px-4 pt-4 lg:px-8 lg:pt-8">
               <img src="./all_candy.png" alt="" className="rounded-xl" />
             </figure>
-            <h3 className="card-title"> Show All Candy</h3>
+            <h3 className="card-title text-lg lg:text-xl"> Show All Candy</h3>
           </div>
         </div>
       </div>
